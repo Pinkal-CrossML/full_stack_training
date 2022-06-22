3c410f7ca396d7afe2a221597e130fc2472ca3c4
pinkal <pinkal@crossml.com>
2022-06-22 12:30:21 +0530

    added models

--- Rannsolve/Process/models.py
+++ Rannsolve/Process/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Dept(models.Model):
+    dept_id = models.CharField(primary_key='True',max_length=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
