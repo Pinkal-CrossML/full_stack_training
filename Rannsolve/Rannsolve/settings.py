4b21d9f55cfd6d21ab091d300a0ad84753f81b07
pinkal <pinkal@crossml.com>
2022-06-28 12:44:49 +0530

    fixd the inputs

--- Rannsolve/Rannsolve/settings.py
+++ Rannsolve/Rannsolve/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Process'
+    'Process',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
