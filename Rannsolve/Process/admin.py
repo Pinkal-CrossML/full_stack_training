086529ee4041ec9a3a8eaf5d275a81394f100658
pinkal <pinkal@crossml.com>
2022-07-01 15:09:03 +0530

    first commit

--- Rannsolve/Process/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Process_data, Cnn_model,auth_user
-# Register your models here.
-admin.site.register(Process_data)
-admin.site.register(Cnn_model)
-admin.site.register(auth_user)
